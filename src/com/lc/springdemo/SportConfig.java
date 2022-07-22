b610485f0a3caf97f4e61d60474e388f377687aa
Alexander Kravchenko <alex.kravchenko@protonmail.com>
2022-07-22 14:23:15 +0300

    progress update

--- src/com/lc/springdemo/SportConfig.java
+++ src/com/lc/springdemo/SportConfig.java
@@ -3,9 +3,11 @@ package com.lc.springdemo;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 //@ComponentScan("com.lc.springdemo")
+@PropertySource("classpath:sport.properties")
 public class SportConfig {
 	//define bean for sad forune service
 	@Bean
--- src/com/lc/springdemo/SwimJavaConfigDemoApp.java
+++ src/com/lc/springdemo/SwimJavaConfigDemoApp.java
@@ -9,10 +9,12 @@ public class SwimJavaConfigDemoApp {
 		AnnotationConfigApplicationContext context = 
 				new AnnotationConfigApplicationContext(SportConfig.class);
 		//get the bean from Spring container
-		Coach theCoach = context.getBean("swimCoach", Coach.class);
+		SwimCoach theCoach = context.getBean("swimCoach", SwimCoach.class);
 		//call a method on the bean
 		System.out.println(theCoach.getDailyWorkout());
 		System.out.println(theCoach.getDailyFortune());
+		System.out.println(theCoach.getTeam());
+
 		//close the context
 		context.close();
 	}
